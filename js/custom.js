49894b4716ce7b91d96bf33cf75518c9a5b46251
MarkzMapiloko <49343431+MarkzMapiloko@users.noreply.github.com>
2020-01-23 18:09:05 -0800

    Added Navigation

--- js/custom.js
+++ js/custom.js
@@ -63,3 +63,31 @@ $(function () {
         autoplayHoverPause: true
     });
 });
+/*======================================================
+                        Navigation
+========================================================*/
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 50) {
+            //Hide Nav
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+
+        } else {
+            //show Nav
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+
+        }
+    });
+});
+
+$(function () {
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        var section = $(this).attr("href");
+        $('html,body').animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+}); 
